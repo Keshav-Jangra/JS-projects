ab884835b93aa21a408f80ed9b8e8c7c11207c8f
Keshav <keshav.k098765@gmail.com>
2026-08-31 19:55:51 +0530

    Implement localStorage for game data persistence
    Added functionality to save and retrieve game data from localStorage, including total games played and wins.

--- memoryGame.js
+++ memoryGame.js
@@ -1,19 +1,38 @@
+let savedData = localStorage.getItem("memoryData");
+if (savedData !== null) {
+  data = JSON.parse(savedData);
+}
+
 const container = document.getElementById("cards");
 let moves = document.getElementById("moves");
 let time = document.getElementById("time");
 let countMoves = 0;
+let timenow = 0;
 
 let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+numbers.sort(() => Math.random() - 0.5);
 let firstCard = null;
 let secondCard = null;
 let matchedPairs = 0;
 let lock = false;
+let timeFlag = true;
+
+let totalGames = document.getElementById("total-games");
+let totalWins = document.getElementById("total-wins");
+let bestTime = document.getElementById("best-time");
+let lowestMoves = document.getElementById("lowest-moves");
 
 let data = {
   TotalGamePlayed: 0,
   TotalWins: 0,
+  BestTime: 0,
   LowestMoves: 0,
 };
+
+totalGames.textContent = `${data.TotalGamePlayed}`;
+totalWins.textContent = `${data.TotalWins}`;
+bestTime.textContent = `${data.BestTime}`;
+lowestMoves.textContent = `${data.LowestMoves}`;
 
 function createCards() {
   container.innerHTML = "";
@@ -55,6 +74,11 @@ function checking() {
         time = 0;
         countMoves = 0;
         moves.textContent = countMoves;
+        timeFlag = true;
+
+        data.TotalGamePlayed++;
+
+        localStorage.setItem("dataStore", JSON.stringify(data));
         createCards();
       }
     }, 100);
@@ -73,15 +97,29 @@ function checking() {
 
 let reset = document.getElementById("reset");
 reset.addEventListener("click", function () {
+  numbers.sort(() => Math.random() - 0.5);
   firstCard = null;
   secondCard = null;
   matchedPairs = 0;
   lock = 0;
   time = 0;
+  countMoves = 0;
+  moves.textContent = countMoves;
+  timenow = 0;
+  time.textContent = timenow;
+  timeFlag = true;
+
   createCards();
 });
 
 container.addEventListener("click", function (e) {
+  if (timeFlag) {
+    timeFlag = false;
+    setInterval(() => {
+      time.textContent = timenow;
+      timenow = timenow + 1;
+    }, 1000);
+  }
   const card = e.target;
   if (lock) return;
   if (card == firstCard) return;
